482baa613ec4c3b8629b5e477d4fe376b6ce7abb
jules-lucquet <jules.lucquet@gmail.com>
2025-11-04 07:55:01 +0000

    feat: Maitrise des variables et conditions

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
- console.log("Laboratoire prêt !");
- 
+console.log("Laboratoire prêt !");
+
+const nom = "Votre Nom";
+let age = 17;
+
+const motDePasseAttendu = "secret123";
+let motDePasseUtilisateur = "secret123"; // Changez cette valeur pour tester
+
+let longueur = motDePasseUtilisateur.length
+
+if (motDePasseUtilisateur === motDePasseAttendu) {
+    console.log("Accès autorisé. Bienvenue !");
+} else if (longueur < 8) {
+    console.log("Accès refusé. Votre mot de passe est trop court (8 caractères min).");
+} else {
+    console.log("Accès refusé. Mot de passe incorrect.");
+}
+
+const moi = {
+    prenom: "VotrePrénom",
+    nom: "VotreNom",
+    age: 18,
+    competences: ["HTML", "CSS"]
+};
